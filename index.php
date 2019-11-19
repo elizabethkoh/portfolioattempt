2e5540b7250b9f4241489ff965e27650871082be
elizabethkoh <elizabethkoh2911@gmail.com>
2019-11-19 17:20:35 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Elizabeth Koh 2nd Period]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="koh_aboutme.html">AboutMe.html</a></li></br>
+      <li><a href="koh_projects.html">Projects.html</a></li></br>
+      <li><a href="koh_resume.html">Resume.html</a></li></br>
+      <li><a href="koh_contactme.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
